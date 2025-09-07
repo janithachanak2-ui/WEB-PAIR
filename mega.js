bafe862ad8e316d2c4d20b817e26348f30e22bd5
janitha <janithachanak2@gmail.com>
2025-09-07 11:48:37 -0700

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'janithachanak2@gmail.com',
+    password: 'NO.GIRLS@#$17',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
